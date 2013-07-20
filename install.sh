80edbc921b0fbce53c7c8efdc97f0a115350fbda
Dan Nuffer <danielnuffer@gmail.com>
2013-07-20 12:11:13 -0600

    Check if ~/.vim or ~/.vimrc already exist

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
+[ -d ~/.vim ] || { echo "~/.vim already exists" >&2; exit 1 }
+[ -d ~/.vimrc ] || { echo "~/.vim already exists" >&2; exit 1 }
 ln -sf $PWD/dotvimrc ~/.vimrc
 ln -sf $PWD/dotvim ~/.vim

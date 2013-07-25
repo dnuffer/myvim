a7eb0188001b21be4a1efdfd7a9fcda3db9ae02f
Dan Nuffer <danielnuffer@gmail.com>
2013-07-24 20:12:01 -0600

    Adjust to if as I understand it better.

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-[ -d ~/.vim ] && { echo "~/.vim already exists" >&2; exit 1; }
-[ -d ~/.vimrc ] && { echo "~/.vim already exists" >&2; exit 1; }
+if [ -d ~/.vim ]; then 
+  echo "~/.vim already exists" >&2
+  exit 1
+fi
+
+if [ -d ~/.vimrc ]; then
+  echo "~/.vim already exists" >&2
+  exit 1
+fi
+
 ln -sf $PWD/dotvimrc ~/.vimrc
 ln -sf $PWD/dotvim ~/.vim

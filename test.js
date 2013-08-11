7bd2319d7b19050343c0cdd0160f90dc3185798f
Dan Nuffer <danielnuffer@gmail.com>
2013-08-11 12:46:45 -0600

    Squashed 'dotvim/bundle/simple-javascript-indenter/' content from commit b1cbd5b
    git-subtree-dir: dotvim/bundle/simple-javascript-indenter
    git-subtree-split: b1cbd5b147b15245b02c5a9a6789a06950de6482

---
+++ test.js
@@ -0,0 +1,236 @@
+/*
+ * Comment Test 
+ * function() {
+ *
+ */
+(a / b); // c
+'' + "'" + // ' (
+{
+  {
+    console.info((function() {
+    }).something);
+    next();
+  }
+
+  foo (a(b,
+    c)) {
+    aoeu
+  }
+
+  a = "hello \
+  world"
+
+  /hello(world/
+  a + /aoe(uaoeu/
+  (a / b); // c ( /
+  (a / b); // (
+  /a\/b/ // c (
+  world
+
+  var rinput = /^(?:color|date|datetime|email|hidden|month|number|password|range|search|tel|text|time|url|week)$/i,
+      // #7653, #8125, #8152: local protocol detection
+      rlocalProtocol = /(?:^file|^widget|\-extension):$/,
+      /* comment 2 */
+      assigment2;
+
+  /* a [{( */
+  // b [{(
+  comment('/* com', a /* [{( */); /* c */ // d
+  ok();
+  // Assign Test
+  var a, /* { */
+      b, // [
+      /reg/,
+      c = '{'
+  var a = "Hello"
+  (3+4) +
+  "World"
+  return a,
+         b,
+         c;
+  d = function() {
+    aoeu
+  }
+
+  function a() {
+    test(["hello",
+      "world"
+    ])
+  }
+  var k = function() {
+    var k=3,
+        m=4
+  }
+
+  var m = new SWFUpload({
+    debug: false
+  });
+
+  var rurl = /{[('"/,
+      r20 = /%20/g,
+
+  a = 1,
+  b = 2
+  a = 1 +
+      2 *
+      3
+
+  // Function nested
+  ;(function($) {
+    $(document).ready(function() {
+      $('#foo').click(function() {
+        $.post(url, {
+          a: 1, 
+          b: 2,
+        }, function() {
+          ok(); 
+        });
+      });
+    });
+  })(jQuery);
+
+  // Array Object Test
+  string_test("('",'("',"[",'{',"\"{","\\'{"); 
+  {
+    array_object_test: [
+      1,
+      2,
+      {
+        a: [3,4],
+        b: [
+          3,
+          4
+        ],
+        c: string_test("('",'("',"[",'{',"\"{","\\'{"),
+        function_test: function() {
+          return 0;
+        },
+        one_line_function_test: function() { return [1] }
+      }
+    ]
+  }
+
+  // One Line Test
+  {
+    if( a == b && // comment
+      c == d ||
+      e == f) 
+    {
+      ok();
+    }
+    if(a) {
+      b;
+    } else {
+      e;
+    }
+    if (a)
+      b;
+    if(a)
+      b;
+    else if(k) {
+      aeou
+    } else
+      c;
+    while(true)
+      foo += 1;
+    try
+      a;
+    catch
+      b;
+    finally
+      c;
+    ok();
+  }
+
+  query = query.replace(/\=\s*([^'"\]]*)\s*\]/g, "='$1']");
+  // Only use querySelectorAll on non-XML documents
+  {
+    for ( var type in Expr.match ) {
+      Expr.match[ type ] = new RegExp( Expr.match[ type ].source + (/(?![^\[]*\])(?![^\(]*\))/.source) );
+      Expr.leftMatch[ type ] = new RegExp( /(^(?:.|\r|\n)\/*?)/.source + Expr.match[ type ].source.replace(/\\(\d+)/g, fescape) );
+    }
+  }
+  {
+    add: function( handleObj ) {
+      jQuery.event.add( this,
+        liveConvert( handleObj.origType, handleObj.selector ),
+        jQuery.extend({}, handleObj, {handler: liveHandler, guid: handleObj.handler.guid}) ); 
+    },
+    hello()
+  }
+
+  {
+    if ( jQuery.isWindow( elem ) ) {
+    } else if ( elem.nodeType === 9 ) {
+      // Either scroll[Width/Height] or offset[Width/Height], whichever is greater
+      return Math.max(
+        elem.documentElement["client" + name],
+        elem.body["scroll" + name], elem.documentElement["scroll" + name],
+        elem.body["offset" + name], elem.documentElement["offset" + name]
+      );
+
+      // Get or set width or height on the element
+    }
+
+    function a() {
+      $('"', {'b'}, function() {
+      });
+    }
+
+    test(//aoeuaou{
+      hello();
+    );
+    test(/*a'*/,'ee') {
+      foo();
+    }
+    function escapeattr(b) {
+      return (/./i, "") : ""
+    }
+
+    switch (foo) {
+      case a: break;
+
+      case b: 
+      break;
+
+      case c: {
+        break;
+      }
+
+      case d: {
+        switch (bar) {
+          case e:
+          break;
+
+          default:
+          break;
+        }
+      }
+
+      default:
+      break;
+    }
+  }
+}
+
+var a = a
+  , b = b
+  , c = c;
+
+var a = a
+  , b = b
+  ;
+
+foo({
+    a : a
+  , b : b
+  , c : c
+  , d : {
+        e : e
+        f : f
+    }
+  , g : {[
+        h : h
+      , i : i
+    ]}
+});
